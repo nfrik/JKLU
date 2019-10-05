239b2da4a0d52b2bcbf2bac43431aabacc8e7665
Nikolay <nfrik@emetx.com>
2019-10-05 10:08:36 -0400

    quick test on ccs format la4j and mtj

--- src/test/java/edu/ufl/cise/klu/test/LUtest.java
+++ src/test/java/edu/ufl/cise/klu/test/LUtest.java
@@ -4,6 +4,7 @@ import edu.ufl.cise.klu.common.KLU_common;
 import edu.ufl.cise.klu.common.KLU_numeric;
 import edu.ufl.cise.klu.common.KLU_symbolic;
 import edu.ufl.cise.klu.utils.CCSMatrixWrap;
+import no.uib.cipr.matrix.Matrix;
 import no.uib.cipr.matrix.sparse.CompColMatrix;
 import no.uib.cipr.matrix.sparse.FlexCompColMatrix;
 import no.uib.cipr.matrix.sparse.SparseVector;
@@ -36,14 +37,81 @@ public class LUtest {
      * a simple KLU demo; solution is x = (1,2,3,4,5)
      */
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void mtj_ccs_klu_test(){
+        long startTime = System.nanoTime();
 
-        SparseMatrix sparseMatrix = CCSMatrix.zero(n, n);
-//        SparseMatrix sparseMatrix = CCSMatrix.randomSymmetric(n,0.05,new Random());
+        FlexCompColMatrix sparseMatrix = new FlexCompColMatrix(n,n);
+        CompColMatrix tst = new CompColMatrix(sparseMatrix);
+
+        System.out.println("Matrix instantiated time msec: "+ (System.nanoTime()-startTime)/1000000);
+
+        startTime = System.nanoTime();
 
         b= new double[n];
 
         Random rand = new Random();
+        rand.setSeed(1212);
+
+        for(int i=0;i<sparseMatrix.numColumns();i++){
+            for(int m=0;m<rand.nextInt(50);m++){
+                int k = rand.nextInt(sparseMatrix.numRows());
+                double val = rand.nextDouble();
+                sparseMatrix.set(i,k,val);
+                sparseMatrix.set(k,i,val);
+                sparseMatrix.set(i,i,rand.nextDouble());
+            }
+
+            b[i]=rand.nextDouble();
+        }
+        System.out.println("Matrix initialized time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
+
+        CompColMatrix ccsm = new CompColMatrix(sparseMatrix);
+
+//        new CompColMatrix()
+
+        Ap= ccsm.getColumnPointers();
+        Ai= ccsm.getRowIndices();
+        Ax= ccsm.getData();
+
+        System.out.println("Retrieved CCS data time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
+
+        KLU_symbolic Symbolic;
+        KLU_numeric Numeric;
+        KLU_common Common = new KLU_common();
+
+        //Dklu_version.NPRINT = false ;
+        //Dklu_internal.NDEBUG = false ;
+
+        klu_defaults (Common);
+
+
+        Symbolic = klu_analyze (n, Ap, Ai, Common);
+        System.out.println("Klu analyzed time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
+        Numeric = klu_factor (Ap, Ai, Ax, Symbolic, Common);
+        System.out.println("Klu factorized time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
+        klu_solve (Symbolic, Numeric, n, 1, b, 0, Common);
+
+        System.out.println("Klu solved time msec: "+ (System.nanoTime()-startTime)/1000000);
+        System.out.println("Peak memory:"+ Common.mempeak+" status "+Common.status);
+
+    }
+
+    public static void la4j_ccs_klu_test(){
+        long startTime = System.nanoTime();
+
+        SparseMatrix sparseMatrix = CCSMatrix.zero(n, n);
+        System.out.println("Matrix instantiated time msec: "+ (System.nanoTime()-startTime)/1000000);
+
+        startTime = System.nanoTime();
+
+        b= new double[n];
+
+        Random rand = new Random();
+        rand.setSeed(1212);
 
         for(int i=0;i<sparseMatrix.rows();i++){
             for(int m=0;m<rand.nextInt(50);m++){
@@ -56,17 +124,16 @@ public class LUtest {
 
             b[i]=rand.nextDouble();
         }
+        System.out.println("Matrix initialized time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
 
-        DenseMatrix dm = sparseMatrix.toDenseMatrix();
-//        System.out.println(dm);
         Ap= CCSMatrixWrap.getColumnPointers(sparseMatrix);
         Ai= CCSMatrixWrap.getRowIndices(sparseMatrix);
         Ax= CCSMatrixWrap.getValues(sparseMatrix);
 
-//        sparseMatrix.set(0,0,23f);
 
-        FlexCompColMatrix a = new FlexCompColMatrix(n,n);
-        CompColMatrix ccm = new CompColMatrix(a);
+        System.out.println("Retrieved CCS data time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
 
         KLU_symbolic Symbolic;
         KLU_numeric Numeric;
@@ -77,18 +144,26 @@ public class LUtest {
 
         klu_defaults (Common);
 
-        long startTime = System.nanoTime();
+
         Symbolic = klu_analyze (n, Ap, Ai, Common);
-        System.out.println("Execution time msec: "+ (System.nanoTime()-startTime)/1000000);
+        System.out.println("Klu analyzed time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
         Numeric = klu_factor (Ap, Ai, Ax, Symbolic, Common);
-        System.out.println("Execution time msec: "+ (System.nanoTime()-startTime)/1000000);
+        System.out.println("Klu factorized time msec: "+ (System.nanoTime()-startTime)/1000000);
+        startTime = System.nanoTime();
         klu_solve (Symbolic, Numeric, n, 1, b, 0, Common);
 
-        System.out.println("Execution time msec: "+ (System.nanoTime()-startTime)/1000000);
+        System.out.println("Klu solved time msec: "+ (System.nanoTime()-startTime)/1000000);
         System.out.println("Peak memory:"+ Common.mempeak+" status "+Common.status);
 //        for (i = 0 ; i < n ; i++) {
 //            System.out.printf("x [%d] = %g\n", i, b [i]) ;
 ////            assertEquals(i + 1.0, b [i], DELTA) ;
 //        }
+    }
+
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        mtj_ccs_klu_test();
+        la4j_ccs_klu_test();
     }
 }
